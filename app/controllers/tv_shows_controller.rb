7a5bf7c18e908bd9c01ded511fb906a5325f5dbb
Neetesh Gupta <neetesh.gupta95@gmail.com>
2020-02-22 15:33:14 +0530

    marked as favourite

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -1,5 +1,23 @@
 class TvShowsController < ApplicationController
+  before_action :find_tv_show, only: :favourite
+
   def index
   	@tv_shows = TvShow.required_data(params[:q])
+  end
+
+  def favourite
+  	fav = Favourite.new(user_id: current_user.id, tv_show_id: @tv_show.id)
+  	if fav.save
+  		flash[:success] = 'Successfully marked as favourite'
+  	else
+      flash[:notice] = 'Could not marked as favourite'
+  	end
+  	redirect_to tv_shows_path
+  end
+
+  private
+
+  def find_tv_show
+  	@tv_show = TvShow.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do  
-  resources :tv_shows, only: :index
+  resources :tv_shows, only: :index do
+  	get :favourite, on: :member
+  end
     
   devise_for :users
 end
